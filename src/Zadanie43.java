1e1107664f69db125d3180a19166b2c7373effb6
Domi-Szopeu <gemmei.no.01@gmail.com>
2022-01-02 13:31:37 +0100

    Zadanie43

---
+++ src/Zadanie43.java
@@ -0,0 +1,24 @@
+public class Zadanie43 {
+//    Napisz program, który w zadeklarowanej tablicy dwuwymiarowej 10×10
+//    o nazwie macierz umieszcza na przekątnej liczbę 1, a poza przekątną — 0
+//            (zobacz reprezentację graficzną tablicy na sąsiedniej stronie). Dodatkowo
+//    program powinien obliczać sumę elementów wyróżnionych w tablicy,
+//    tj. tych znajdujących się na jej przekątnej.
+    public static void main(String[] args) {
+        int n = 10,x,y, suma;
+        suma = 0;
+        int tab[][] = new int[n][n];
+        for (x = 0; x < tab.length; x++) {
+            for (y = 0; y < tab.length; y++) {
+                if (x == y){
+                    tab[x][y] = 1;
+                    suma = suma + tab[x][y];
+                }
+                else tab[x][y] = 0;
+                System.out.print(tab[x][y] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("Suma to: " + suma);
+    }
+}
